11b263562b3fe4a6e7533ba29e73922260326dd4
Mester0723 <postmatoric@gmail.com>
2025-04-19 15:50:27 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+    
